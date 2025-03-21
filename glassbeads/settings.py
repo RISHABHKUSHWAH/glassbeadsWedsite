6c2b74be62a1dfb3a99c13ab59b3438744e14490
RISHABHKUSHWAH <rishabhkushwah208@gmail.com>
2025-03-21 16:44:33 +0530

    create media dic and add photo,change in admin.py file

--- glassbeads/settings.py
+++ glassbeads/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR /'static',
 ]
+
+
+#media files configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
