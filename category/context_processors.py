0bb598e94ba95df330b006bd20680867301f6971
RISHABHKUSHWAH <rishabhkushwah208@gmail.com>
2025-03-27 23:58:39 +0530

    add category menu (dynamic menu)

---
+++ category/context_processors.py
@@ -0,0 +1,5 @@
+# from django.shortcuts import render
+from .models import Category
+def menu_linksList(request):
+    links = Category.objects.all()
+    return dict(links= links)
--- store/views.py
+++ store/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
+from category.models import Category
 # Create your views here.
 
-def store(request, category_slug=None):
-    products = Product.objects.all().filter(is_available = True)
-    product_count = products.count()
-    # categories = None
-    # products   = None
-    # if category_slug !=None:
-    #     categories = get_object_or_404(Category, category_slug=slug)
-    #     products = Product.objects.all().filter(category=categories,is_available=True)
+def store(request, category_slugs=None):
+    categories = None
+    products   = None
+    if category_slugs !=None:
+        categories = get_object_or_404(Category, slug=category_slugs)
+        products = Product.objects.all().filter(category=categories,is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available = True)
+        product_count = products.count()
     context = {
         'products' : products,
         'product_count' : product_count,
