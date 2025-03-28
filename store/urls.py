d8e0b0e970533cd48ecc11869a90e1d039eea092
RISHABHKUSHWAH <rishabhkushwah208@gmail.com>
2025-03-28 15:56:06 +0530

    single product detail html page and make get_url(add in pages)

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.store,name='store'),
-    path('<slug:category_slugs>/',views.store, name='products_by_category')
+    path('<slug:category_slugs>/',views.store, name='products_by_category'),
+    path('<slug:category_slugs>/<slug:product_slug>/',views.product_detail, name='product_detail'),
 ]
