e30787eca0948a36b97dfdb847b34292361cd79a
RISHABHKUSHWAH <rishabhkushwah208@gmail.com>
2025-03-29 10:53:28 +0530

    create cart page , increase and decrease  item quantity

--- glassbeads/urls.py
+++ glassbeads/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     path('store/',include('store.urls')),
+    path('cart/',include('carts.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
