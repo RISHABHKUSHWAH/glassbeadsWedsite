2a089eacbd4eebc3e6060ffbeb6dab87942765fa
RISHABHKUSHWAH <rishabhkushwah208@gmail.com>
2025-03-31 00:28:27 +0530

    show cart added item on the cart icon

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponseGone
 # Create your views here.
 
 def store(request, category_slugs=None):
@@ -22,9 +25,13 @@ def store(request, category_slugs=None):
 def product_detail(request,category_slugs,product_slug):
     try:
         single_product = Product.objects.get(category__slug = category_slugs,slug = product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request),product = single_product).exists()
+        # return HttpResponseGone(in_cart)
+        # exit()
     except Exception as e:
         raise e
     context = {
         'single_product':single_product,
+        'in_cart': in_cart,
     }
     return render(request,'store/product_detail.html',context)
